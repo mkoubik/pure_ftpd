de87e1a3bed7b0ced943121fca4cb4f5853ceb20
Gudleik Rasch <gudleik@gmail.com>
2012-06-21 23:22:00 +0200

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Skalar AS"
+maintainer_email "gr@skalar.no"
+license          "All rights reserved"
+description      "Installs/Configures pure_ftpd"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: pure_ftpd
+# Recipe:: default
+#
+# Fork & use as you like
+#
+
+package "pure-ftpd-mysql"
+
+group "ftpgroup"
+
+user "ftpuser" do
+  comment "Virtual pureFTPd Account"
+  gid "ftpgroup"
+  home "/bin/null"
+  shell "/bin/false"
+end
+
+%w(ChrootEveryone CreateHomeDir DontResolve NoAnonymous).each do |file|
+  file "/etc/pure-ftpd/conf/#{file}" do
+    content "yes"
+  end
+end
+
+file "/etc/pure-ftpd/conf/ForcePassiveIP" do
+  content node.has_attribute?("ec2") ? node[:ec2][:public_ipv4] : node[:ipaddress]
+end
+
+file "/etc/pure-ftpd/conf/PassivePortRange" do
+  content "60010 60030"
+end
+
+service "pure-ftpd-mysql" do
+  supports :status => true, :restart => true
+  action[:enable,:start]
+end
+ 
+template "/etc/pure-ftpd/db/mysql.conf" do
+  source "mysql.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(
+    :mysql_user => node[:pure_ftpd][:mysql_user],
+    :mysql_password => node[:pure_ftpd][:mysql_password],
+    :mysql_database => node[:pure_ftpd][:mysql_database],
+    :mysql_server => node[:pure_ftpd][:mysql_server],
+    :mysql_port => node[:pure_ftpd][:mysql_port]
+  )
+  notifies :restart, resources(:service => "pure-ftpd-mysql")
+end
